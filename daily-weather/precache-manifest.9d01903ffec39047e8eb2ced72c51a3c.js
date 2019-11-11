a6d57e066f58bc559335101cd221072eb5054bb3
wizardBr <florentino.flavioweb@gmail.com>
2019-11-10 22:45:28 -0300

    DN

---
+++ daily-weather/precache-manifest.9d01903ffec39047e8eb2ced72c51a3c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80c122e228a49fc82760b3854ae8ecb",
+    "url": "/daily-weather/index.html"
+  },
+  {
+    "revision": "2cfa43b5177ce489ba04",
+    "url": "/daily-weather/static/css/main.c75131ce.chunk.css"
+  },
+  {
+    "revision": "b667307cbb04f3fdd6f2",
+    "url": "/daily-weather/static/js/2.57ba0e11.chunk.js"
+  },
+  {
+    "revision": "2cfa43b5177ce489ba04",
+    "url": "/daily-weather/static/js/main.6a6b7b4f.chunk.js"
+  },
+  {
+    "revision": "4b9df80f45801c6a01b6",
+    "url": "/daily-weather/static/js/runtime-main.7df63b38.js"
+  },
+  {
+    "revision": "8e3fdc70372ee2674e7c82ec6bcb89ad",
+    "url": "/daily-weather/static/media/clouds.8e3fdc70.svg"
+  },
+  {
+    "revision": "b2c36c2a20e69e7947cf69ddb35e7172",
+    "url": "/daily-weather/static/media/error.b2c36c2a.svg"
+  },
+  {
+    "revision": "d89527ca6874a0795cbd58441a559c66",
+    "url": "/daily-weather/static/media/place.d89527ca.svg"
+  },
+  {
+    "revision": "5fcdec4dadf12c39bfdf409a2e8391e3",
+    "url": "/daily-weather/static/media/rain.5fcdec4d.svg"
+  },
+  {
+    "revision": "fa3ed0fe687fae679a90632ce7daf300",
+    "url": "/daily-weather/static/media/refresh.fa3ed0fe.svg"
+  },
+  {
+    "revision": "c204fa6b37777f50099c5b62eabe4b7d",
+    "url": "/daily-weather/static/media/snowflake.c204fa6b.svg"
+  },
+  {
+    "revision": "381339ecf9ca80876e4d0756ce80462d",
+    "url": "/daily-weather/static/media/storm.381339ec.svg"
+  },
+  {
+    "revision": "849e970e8ff7fb60320f9e3172b721b4",
+    "url": "/daily-weather/static/media/sun.849e970e.svg"
+  }
+]);
